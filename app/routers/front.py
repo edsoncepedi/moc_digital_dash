e41335b14ee4fa2dbefbe4ce860704e27a519e10
edsoncepedi <edson.alves@cepedi.org.br>
2026-03-09 11:42:55 -0300

    nova requisição para evitar cache antigo

--- app/routers/front.py
+++ app/routers/front.py
@@ -9,7 +9,15 @@ templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 @router.get("/")
 async def projetor(request: Request):
-    return templates.TemplateResponse(
+
+    response = templates.TemplateResponse(
         "projetor.html",
         {"request": request}
     )
+
+    # Desativa cache
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+
+    return response
